0de4545cf4aace9e758f1414bedf5142e6efeb26
Lilian Schall <lilian.schall@epita.fr>
2023-11-28 12:00:52 +0100

    feat: added base types for messages sent from Wingman to Discussion

---
+++ src/types/WingmanMessage.tsx
@@ -0,0 +1,15 @@
+export type UserMessageAction = {
+    buttonText: string,
+    messageToSend: string,
+};
+
+export type WingmanMessageOption = {
+    embeddedUrl: string | null,
+    acceptAction: UserMessageAction | null,
+    denyAction: UserMessageAction | null,
+};
+
+export type WingmanMessage = {
+    message: string,
+    option: WingmanMessageOption | null,
+};
